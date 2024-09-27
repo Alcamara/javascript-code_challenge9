4ac14147b69343d0252f64a51402f96ff8da0693
alcamara <alhousseinec@gmail.com>
2024-09-26 20:34:22 -0500

    added getListItems and addListItem functions

--- server/implementation/to-do.js
+++ server/implementation/to-do.js
@@ -1,1 +1,19 @@
 const { listItem } = require('../model/to-do')
+
+const list = []
+
+function getListItem(){
+    return list
+}
+
+function addListItem(id, note, isComplete){
+    const prevListLength = list.length
+    const item = listItem(id, note, isComplete)
+    if(list.length > prevListLength){
+        return 200
+    }
+
+    return 500
+}
+
+module.exports = { addListItem, getListItem }
--- server/server.js
+++ server/server.js
@@ -3,9 +3,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+
+
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 
 app.listen(PORT, () => {
     console.log(`listening on localhost:5000`)
+})
+
+app.get("/", (req, res) => {
+    res.send('hello world')
 })
