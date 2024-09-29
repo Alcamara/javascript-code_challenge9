dbe24437c12ba40568ecea6c9fcc9601a5d1f17e
Cantrell24 <caandrews18@gmail.com>
2024-09-29 11:23:45 -0400

    Add get route for todo list

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ app.get("/",(req, res) =>{
     res.send("Hello World")
 })
 
+app.get("/todo", (req, res) =>(
+    res.send(["Run", "Shopping", "Do dishes", "Cut Grass"])
+))
+
 app.listen(PORT, () =>{
     console.log("Listening on Port 3000")
 })
